0a07e41bbeea803376c7acf6bd2dc17d0ef3a783
dance2die <dontbothermewithspam@gmail.com>
2016-03-16 15:36:47 -0400

    skeletal code

---
+++ Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/defaultCtrl.js
@@ -0,0 +1,12 @@
+﻿(function ($, undefined) {
+
+	angular.module("exampleApp", [])
+		.controller("defaultCtrl", function ($scope) {
+			$scope.products = [
+				{ name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
+				{ name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
+				{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 }
+			];
+		});
+
+})(jQuery);
