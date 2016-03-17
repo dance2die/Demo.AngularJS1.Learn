07e0813ae080470ebc5df8a89a4967b453beaba8
dance2die <dontbothermewithspam@gmail.com>
2016-03-17 12:37:16 -0400

    removed data property dependency

--- Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/defaultCtrl.js
+++ Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/defaultCtrl.js
@@ -4,12 +4,14 @@
 		.directive("unorderedList", function() {
 			return function(scope, element, attrs) {
 				var data = scope[attrs["unorderedList"]];
+				var listProperty = attrs["listProperty"];
+
 				if (angular.isArray(data)) {
 					var listElem = angular.element("<ul>");
 					element.append(listElem);
 
 					for (var i = 0; i < data.length; i++) {
-						listElem.append(angular.element("<li>").text(data[i].name));
+						listElem.append(angular.element("<li>").text(data[i][listProperty]));
 					}
 				}
 			};
