5c3921f86abcf8bd6126ef760c9b5df183bde416
dance2die <dontbothermewithspam@gmail.com>
2016-03-17 16:22:03 -0400

    No text is appended.

--- Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/defaultCtrl.js
+++ Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/defaultCtrl.js
@@ -11,8 +11,16 @@
 					element.append(listElem);
 
 					for (var i = 0; i < data.length; i++) {
-						var text = scope.$eval(propertyExpression, data[i]);
-						listElem.append(angular.element("<li>").text(text));
+						var itemElement = angular.element("<li>");
+						listElem.append(itemElement);
+
+						var watcherFn = function(watchScope) {
+							return watchScope.$eval(propertyExpression, data[i]);
+						};
+
+						scope.$watch(watcherFn, function(newValue, oldValue) {
+							itemElement.text(newValue);
+						});
 					}
 				}
 			};
