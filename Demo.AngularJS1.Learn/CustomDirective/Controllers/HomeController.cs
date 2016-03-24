65e8204911c7bcdcaa9849220a731c3fee48790f
dance2die <dontbothermewithspam@gmail.com>
2016-03-24 13:13:06 -0400

    transclusion demo

--- Demo.AngularJS1.Learn/CustomDirective/Controllers/HomeController.cs
+++ Demo.AngularJS1.Learn/CustomDirective/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CustomDirective.Controllers
 {
@@ -15,6 +11,11 @@ namespace CustomDirective.Controllers
         }
 
 	    public ActionResult ScopeControl()
+	    {
+		    return View();
+	    }
+
+	    public ActionResult Transclusion()
 	    {
 		    return View();
 	    }
