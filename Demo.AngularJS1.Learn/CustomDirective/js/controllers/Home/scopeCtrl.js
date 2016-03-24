e4330cedcef76bb2286b77c31f1ac3b9108f0e33
dance2die <dontbothermewithspam@gmail.com>
2016-03-24 09:04:29 -0400

    give directive its own scope so when directive is shared, models are not shared.

--- Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/scopeCtrl.js
+++ Demo.AngularJS1.Learn/CustomDirective/js/controllers/Home/scopeCtrl.js
@@ -3,7 +3,8 @@
 	angular.module("exampleApp", [])
 		.directive("scopeDemo", function() {
 			return {
-				template: "<div class='panel-body'>Name: <input ng-model=name /></div>"
+				template: "<div class='panel-body'>Name: <input ng-model=name /></div>",
+				scope: true
 			}
 		})
 		.controller("scopeCtrl", function ($scope) {
