d9d71dd6ef7d254781818394e10da9f30d9e7fdb
dance2die <dontbothermewithspam@gmail.com>
2016-03-25 14:57:45 -0400

    logging using logService created using factory

--- Demo.AngularJS1.Learn/Services/js/controllers/Home/defaultCtrl.js
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/defaultCtrl.js
@@ -1,13 +1,14 @@
 ﻿(function (undefined) {
-	angular.module("exampleApp", ["customDirectives"])
-		.controller("defaultCtrl", ["$scope", function ($scope) {
+	angular.module("exampleApp", ["customDirectives", "customServices"])
+		.controller("defaultCtrl", ["$scope", "logService", function ($scope, logService) {
 			$scope.data = {
 				cities: ["London", "New York", "Paris"],
 				totalClicks: 0
 			};
 
 			$scope.$watch("data.totalClicks", function (newVal) {
-				console.log("Total Click Count: " + newVal);
+//				console.log("Total Click Count: " + newVal);
+				logService.log("Total click count: " + newVal);
 			});
 		}]);
 })();
---
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/services.js
@@ -0,0 +1,12 @@
+﻿(function(undefined) {
+	angular.module("customServices", [])
+		.factory("logService", function() {
+			var messageCount = 0;
+
+			return {
+				log: function(message) {
+					console.log("(LOG + " + messageCount++ + ") " + message);
+				}
+			};
+		});
+})();
