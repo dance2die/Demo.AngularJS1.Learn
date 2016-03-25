f5eca5d0d31142a78eeb835bd426c40ae298aaaa
dance2die <dontbothermewithspam@gmail.com>
2016-03-25 12:59:45 -0400

    example project prepared

---
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/defaultCtrl.js
@@ -0,0 +1,28 @@
+﻿(function(undefined) {
+	angular.module("exampleApp", [])
+		.controller("defaultCtrl", ["$scope", function($scope) {
+			$scope.data = {
+				cities: ["London", "New York", "Paris"],
+				totalClicks: 0
+			};
+
+			$scope.$watch("data.totalClicks", function(newVal) {
+				console.log("Total Click Count: " + newVal);
+			});
+		}])
+		.directive("triButton", function() {
+			return {
+				scope: {
+					counter: "=counter"
+				},
+				link: function(scope, element, attrs) {
+					element.on("click", function(event) {
+						console.log("Button click: " + event.target.innerText);
+						scope.$apply(function() {
+							scope.counter++;
+						});
+					});
+				}
+			};
+		});
+})();
