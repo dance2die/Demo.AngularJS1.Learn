a1b8b3d55887709194214485c9eeab0f7aa29b93
dance2die <dontbothermewithspam@gmail.com>
2016-03-25 15:07:52 -0400

    logService using service method

--- Demo.AngularJS1.Learn/Services/js/controllers/Home/services.js
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/services.js
@@ -1,12 +1,21 @@
 ﻿(function(undefined) {
 	angular.module("customServices", [])
-		.factory("logService", function() {
-			var messageCount = 0;
+		//.factory("logService", function() {
+		//	var messageCount = 0;
 
+		//	return {
+		//		log: function(message) {
+		//			console.log("(LOG + " + messageCount++ + ") " + message);
+		//		}
+		//	};
+		//});
+		.service("logService", function() {
 			return {
+				messageCount: 0,
 				log: function(message) {
-					console.log("(LOG + " + messageCount++ + ") " + message);
+					console.log("(LOG2 + " + (this.messageCount)++ + ") " + message);
 				}
 			};
 		});
+
 })();
