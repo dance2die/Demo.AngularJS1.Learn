7c8ea4a3a0b4993997c0db12771f97a6183c97e4
dance2die <dontbothermewithspam@gmail.com>
2016-03-25 14:51:28 -0400

    Modularized directives

---
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/customDirectives.js
@@ -0,0 +1,18 @@
+﻿(function(undefined) {
+	angular.module("customDirectives", [])
+		.directive("triButton", function () {
+			return {
+				scope: {
+					counter: "=counter"
+				},
+				link: function (scope, element, attrs) {
+					element.on("click", function (event) {
+						console.log("Button click: " + event.target.innerText);
+						scope.$apply(function () {
+							scope.counter++;
+						});
+					});
+				}
+			};
+		})
+})();
--- Demo.AngularJS1.Learn/Services/js/controllers/Home/defaultCtrl.js
+++ Demo.AngularJS1.Learn/Services/js/controllers/Home/defaultCtrl.js
@@ -1,28 +1,13 @@
-﻿(function(undefined) {
-	angular.module("exampleApp", [])
-		.controller("defaultCtrl", ["$scope", function($scope) {
+﻿(function (undefined) {
+	angular.module("exampleApp", ["customDirectives"])
+		.controller("defaultCtrl", ["$scope", function ($scope) {
 			$scope.data = {
 				cities: ["London", "New York", "Paris"],
 				totalClicks: 0
 			};
 
-			$scope.$watch("data.totalClicks", function(newVal) {
+			$scope.$watch("data.totalClicks", function (newVal) {
 				console.log("Total Click Count: " + newVal);
 			});
-		}])
-		.directive("triButton", function() {
-			return {
-				scope: {
-					counter: "=counter"
-				},
-				link: function(scope, element, attrs) {
-					element.on("click", function(event) {
-						console.log("Button click: " + event.target.innerText);
-						scope.$apply(function() {
-							scope.counter++;
-						});
-					});
-				}
-			};
-		});
+		}]);
 })();
