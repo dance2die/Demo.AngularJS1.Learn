30940f60f5b6c206c5a4bc1d6fbf9e69ee7f5565
dance2die <dontbothermewithspam@gmail.com>
2016-04-06 10:16:47 -0400

    added Angular Route to learn about routing

---
+++ Demo.AngularJS1.Learn/Services/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Services.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Exceptions()
+        {
+            return View();
+        }
+
+	    public ActionResult Products()
+	    {
+		    return View();
+	    }
+    }
+}
